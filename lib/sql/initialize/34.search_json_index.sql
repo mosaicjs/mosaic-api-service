805c90502d7589f09d8f5c6f3e3688e728a8bd7c
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2016-07-07 19:09:30 +0200

    Implement AND/OR logic for search in multiple indexes

--- lib/sql/initialize/34.search_json_index.sql
+++ lib/sql/initialize/34.search_json_index.sql
@@ -1,17 +1,32 @@
-CREATE OR REPLACE FUNCTION search_json_index(tbl text, lang text, queries jsonb) RETURNS SETOF json_result_rec AS $$
-    var viewAliases = []; 
+CREATE OR REPLACE FUNCTION search_json_index(tbl text, lang text, intersection boolean, queries jsonb) RETURNS SETOF json_result_rec AS $$
     var array = [];
     Object.keys(queries).forEach(function(idxName, i){
-        var viewAlias = 'T' + i;
-        viewAliases.push(viewAlias);
         var line = getSqlLine(idxName, queries[idxName]);
-        line = '(' + line + ') AS ' + viewAlias;
+        line = '(' + line + ')';
         array.push(line);
     });
-    // "OR" logic
-    var sql = array.join(' UNION ALL ');
-    sql = 'SELECT ROW_NUMBER() OVER (ORDER BY id) AS pos, id FROM (' +
-      ' SELECT distinct(id) AS id FROM ' + sql + ') AS A';
+
+        // "AND" logic
+    var sql; 
+    if (intersection) {
+        var where = [];
+        sql = array.map(function(line, i){
+            if (i > 0) {
+                where.push('(T0.id=T' + i + '.id)');
+            }
+            return line + ' AS T' + i;
+        }).join(', ');
+        if (where.length){
+            sql += ' WHERE ' + where.join(' AND ');   
+        }
+        sql = 'SELECT T0.id FROM ' + sql; 
+        sql = 'SELECT ROW_NUMBER() OVER (ORDER BY id) AS pos, id FROM (' + sql + ') AS B';
+    } else {
+        // "OR" logic
+        sql = array.join(' UNION ALL ');
+        sql = 'SELECT ROW_NUMBER() OVER (ORDER BY id) AS pos, id FROM (' +
+          ' SELECT distinct(id) AS id FROM (' + sql + ') AS A) AS B';
+    }
     // plv8.elog(NOTICE, sql);
     
     return plv8.execute(sql);
@@ -22,5 +37,3 @@ CREATE OR REPLACE FUNCTION search_json_index(tbl text, lang text, queries jsonb)
         "WHERE document @@ to_tsquery('" + query + "')";
     }
 $$ LANGUAGE plv8 STRICT IMMUTABLE
--- select (rec).pos, (rec).id as id from ( select search_json_index('organizations', 'french', '{"q":"web"}'::jsonb) as rec ) as N
-
